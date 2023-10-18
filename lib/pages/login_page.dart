6366b873070f907c24dfd6d95bfbfca02b1fac7b
IT2102154-Priyantha <IT21021534@my.sliit.lk>
2023-10-19 00:56:23 +0530

    feature: InputField

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tour_me/constants.dart';
+import 'package:tour_me/widgets/tablet_text_input.dart';
 
 class LoginPage extends StatelessWidget {
   static const String routeName = '/loginPage';
@@ -6,6 +8,26 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                width: MediaQuery.of(context).size.width / 2,
+                child: Image.asset(MyImages.logo),
+              ),
+              const TabletTextField(
+                hintText: 'Enter Text',
+                keyboardType: TextInputType.emailAddress,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
